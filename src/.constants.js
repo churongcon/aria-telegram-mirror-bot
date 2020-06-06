f4713636728a38c766b86f0d50f367f4d6bc5292
churongcon <verypro.vn@gmail.com>
2020-06-06 20:34:02 +0700

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-  TOKEN: 'bot_token',
+  TOKEN: '1232588497:AAHDJYRJFFnhBPLrLB6segzt1R47nGmv41k',
   ARIA_SECRET: 'hEdHJv%}H%g1ZcCz+jKnA#GyrGU6rm',
   ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
@@ -8,7 +8,7 @@ module.exports = Object.freeze({
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
   INDEX_DOMAIN: 'https://one.driveindex.ga/', // Supporting Sharable Links
   GDRIVE_PARENT_DIR_ID: 'id_of_Drive_folder_to_upload_into',
-  SUDO_USERS: [012, 345],	// Telegram user IDs. These users can use the bot in any chat.
+  SUDO_USERS: [532993493],	// Telegram user IDs. These users can use the bot in any chat.
   AUTHORIZED_CHATS: [678, 901],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
@@ -22,8 +22,8 @@ module.exports = Object.freeze({
     path: '/botNotify'
   },
   COMMANDS_USE_BOT_NAME: {
-    ENABLED: false,  // If true, all commands except '/list' has to have the bot username after the command
-    NAME: "@nameOf_bot"
+    ENABLED: true,  // If true, all commands except '/list' has to have the bot username after the command
+    NAME: "@DoctorPlusTorrentBot"
   },
   IS_TEAM_DRIVE: false
 });
